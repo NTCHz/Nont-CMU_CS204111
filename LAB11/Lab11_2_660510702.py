d9fa1c187bbeb756a2d4ff2bd3b4617c9be2d7f3
NTCHz <Nonny.1122@hotmail.com>
2023-09-12 09:21:10 +0700

    #

---
+++ LAB11/Lab11_2_660510702.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Thichanon Ratanasaenwan (Nont)
+# 660510702
+# Lab11_2
+# 204111 Sec 002
+
+import string
+import time
+
+def my_id():
+    return "660510702"
+
+def main():
+    print(matching_sum((1,), 1))
+    print(matching_sum((5, 2), 7))
+    print(matching_sum((10, -1, 1, -8, 3, 1), 2))
+    print(matching_sum((10, -1, 1, -8, 3, 1), 10))
+    
+
+def matching_sum(t, target_value):
+    t = list(set(t))
+    result = []
+
+    for num in t:
+        complement = target_value - num
+        if complement in t:
+            if num != 0 and complement != 0:
+                print([num, complement])
+
+    print(result)   
+
+
+if __name__ == '__main__':
+    main()
